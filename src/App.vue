cf720ac9a1868c9224eb2f4265e8bb8fbf801325
Nikkei96 <nikitarudentsov@yandex.ru>
2020-09-16 20:59:41 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <PostsForm />
+    <h3>Общее кол-во постов: {{postsCount}}</h3>
+    <div class="post" v-for='post of validPosts' :key='post.id'>
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+import PostsForm from '@/components/PostsForm'
+export default {
+  name: 'App',
+  computed: mapGetters(['validPosts', 'postsCount']),
+  methods: mapActions(['fetchPosts']),
+  mounted() {
+    // this.$store.dispatch('fetchPosts')
+    this.fetchPosts(7)
+  },
+  components: {
+    PostsForm,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  width: 600px;
+}
+
+.post{
+  border: 1px solid #000;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin: 0 0 10px 0;
+}
+</style>
---
+++ src/components/PostsForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <form @submit.prevent='onSubmit'>
+      <label for="title">Title</label>
+      <input type="text" name="" id="title" v-model='title'>
+      <label for="body">Desc</label>
+      <input type="text" name="" id="body" v-model='body'>
+      <button type="submit">Create Post</button>
+    </form>
+    <hr>
+  </div>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+export default {
+  data() {
+    return {
+      title: '',
+      body: '',
+    }
+  },
+  methods: {
+    ...mapMutations(['newPost']),
+    onSubmit() {
+      this.newPost({
+        title: this.title,
+        body: this.body,
+      })
+      this.title = this.body = ''
+    },
+  }
+}
+</script>
+
+<style>
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 400px;
+    border: 1px solid #000;
+    border-radius: 10px;
+    margin: 0 auto 25px;
+    padding: 15px 0;
+  }
+  label {
+    margin: 0 0 5px 0;
+  }
+  input {
+    display: block;
+    width: 50%;
+    margin: 0 0 10px 0;
+  }
+</style>
